9ed03c1c02a07205365fd385710b822d12f1d5b1
sherratt-pemberton <Sherratt285@gmail.com>
2021-09-20 11:03:56 +1200

    completed dom interactions functions

---
+++ DOM-interactions/game.js
@@ -0,0 +1,67 @@
+// Don't change or delete this line! It waits until the DOM has loaded, then calls 
+// the start function. More info: 
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  bindEventListeners(document.getElementsByClassName('board')[0].children)
+}
+
+function bindEventListeners (dots) {
+  for (var i = 0; i < dots.length; i++) {
+    // BIND YOUR EVENT LISTENERS HERE
+    // The first one is provided for you
+    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
+  }
+}
+
+function makeGreen (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('green')
+  updateCounts()
+}
+
+// CREATE FUNCTION makeBlue HERE
+
+function makeBlue(evt){
+  evt.preventDefault()
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
+
+// CREATE FUNCTION hide HERE
+
+function hide (evt){
+  evt.preventDefault()
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
+
+function updateCounts () {
+  var totals = {
+    blue: 0,
+    green: 0,
+    invisible: 0
+  }
+  
+  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
+  var blue = document.getElementsByClassName('blue')
+  totals.blue = blue.length
+
+  var green = document.getElementsByClassName('green')
+  totals.green = green.length
+
+  var invisible = document.getElementsByClassName('invisible')
+  totals.invisible = invisible.length
+
+  displayTotals(totals)
+}
+
+function displayTotals (totals) {
+  for (var key in totals) {
+    document.getElementById(key + '-total').innerHTML = totals[key]
+  }
+}
